7e620ecb9ed4ed600cfb119f229e9b81b53ec9a9
Ajakaiye Afeso <marcusitovie@gmail.com>
2021-03-08 13:21:50 +0100

    Modify initial file

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
     const buttons = document.querySelectorAll("button");
     const image = document.querySelector(".img");
     let counter = 1;
+
     buttons.forEach(button => {
         button.addEventListener("click", function() {
             if ( button.classList.contains("prevbtn") && counter > 1 ) {
                 counter--;
-                image.style.backgroundImage = `url(../image/building${counter}.jpeg)`;
+                image.style.backgroundImage = `url(image/building${counter}.jpeg)`;
             } else if ( button.classList.contains("nextbtn") && counter < 5) {
                 counter++;
-                image.style.backgroundImage = `url(../image/building${counter}.jpeg)`;
+                image.style.backgroundImage = `url(image/building${counter}.jpeg)`;
             }
 
         });
