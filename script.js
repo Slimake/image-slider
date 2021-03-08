447b285bbd70e8dd1a067c45de666adc4fa5a8ab
Ajakaiye Afeso <marcusitovie@gmail.com>
2021-03-08 13:29:11 +0100

    Modify initial file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-(function() {
+function slide() {
     const buttons = document.querySelectorAll("button");
     const image = document.querySelector(".img");
     let counter = 1;
@@ -7,12 +7,14 @@
         button.addEventListener("click", function() {
             if ( button.classList.contains("prevbtn") && counter > 1 ) {
                 counter--;
-                image.style.backgroundImage = `url(/image/building${counter}.jpeg)`;
+                image.style.backgroundImage = `url(image/building${counter}.jpeg)`;
             } else if ( button.classList.contains("nextbtn") && counter < 5) {
                 counter++;
-                image.style.backgroundImage = `url(/image/building${counter}.jpeg)`;
+                image.style.backgroundImage = `url(image/building${counter}.jpeg)`;
             }
 
         });
     });
-})()
+}
+
+slide();
